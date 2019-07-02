464db245078c8b866a232cb85f26d217ec742d54
charguer <arthur@chargueraud.org>
2019-07-02 10:24:49 +0200

    namespace

--- cakeSemantics/Namespace.v
+++ cakeSemantics/Namespace.v
@@ -1,3 +1,4 @@
+From TLC Require Import LibLogic.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Structures.Equalities.
@@ -8,29 +9,8 @@ Definition alist (X Y : Type) := list (X * Y).
 
 (* Comes from somewhere else in Lem semantics *)
 Inductive ident (X:Type) (Y:Type) : Type :=
-| Short : Y -> ident X Y
-| Long : X -> ident X Y -> ident X Y.
-
-Theorem ident_eq_dec : forall (X Y : Type) (i0 i1 : ident X Y),
-    (forall (x0 x1 : X), {x0 = x1} + {x0 <> x1}) ->
-    (forall (y0 y1 : Y), {y0 = y1} + {y0 <> y1}) -> {i0 = i1} + {i0 <> i1}.
-Proof. decide equality.  Qed.
-
-Instance ident_equiv_dec_inst (M N : Type) `(eqm : EqDec M eq) `(eqn : EqDec N eq) : EqDec (ident M N) eq :=
-  { equiv_dec :=
-      fix aux (x y : ident M N) :=
-        match x,y with
-        | Short _ _ n, Short _ _ n' => if equiv_dec n n' then in_left else in_right
-        | Long _ _ m id, Long _ _ m' id' => if equiv_dec m m' then
-                                             if aux id id' then in_left else in_right
-                                           else in_right
-        | _, _ => in_right
-        end
-  }.
-rewrite e. reflexivity. intros C. inversion C. apply (c H0).
-discriminate. discriminate.
-rewrite e, e0. reflexivity. intros C. inversion C. apply (c H1).
-intros C. inversion C. apply (c H0).  Qed.
+  | Short : Y -> ident X Y
+  | Long : X -> ident X Y -> ident X Y.
 
 Arguments Short {X} {Y}.
 Arguments Long {X} {Y}.
@@ -55,63 +35,56 @@ Fixpoint id_to_mods {M N : Type} (id : ident M N) : list M :=
 
 Definition namespace (M N V : Type) := alist (ident M N) V.
 
-Section NamespaceDec.
-  Variable M : Type.
-  Variable N : Type.
-  Variable V : Type.
-  Hypothesis HM : forall (m0 m1 : M), {m0 = m1} + {m0 <> m1}.
-  Hypothesis HN : forall (n0 n1 : N), {n0 = n1} + {n0 <> n1}.
-  Hypothesis HV : forall (v0 v1 : V), {v0 = v1} + {v0 <> v1}.
+(* LATER: replace with the corresponding function from TLC's LibList *)
 
-  Theorem namespace_eq_dec : forall (n0 n1 : namespace M N V), {n0 = n1} + {n0 <> n1}.
-  Proof. decide equality. decide equality. apply ident_eq_dec. apply HM. apply HN.  Qed.
-End NamespaceDec.
-
-Fixpoint lookup {X Y : Type} `{EqDec X} (x : X) (l : alist X Y) : option Y :=
+Fixpoint lookup {X Y : Type} (x : X) (l : alist X Y) : option Y :=
   match l with
   | [] => None
-  | (x',y) :: l' => if equiv_dec x x' then Some y else lookup x l'
+  | (x',y) :: l' => If x = x' then Some y else lookup x l'
   end.
 
-Fixpoint get_modded_namespace {M N V : Type} `{EqDec M} (m : M) (ns : namespace M N V) : namespace M N V :=
+Fixpoint get_modded_namespace {M N V : Type} (m : M) (ns : namespace M N V) : namespace M N V :=
   match ns with
   | [] => []
   | (i,v) :: ns' => match i with
                     | Short _ => get_modded_namespace m ns'
-                    | Long m' i' => if equiv_dec m m'
+                    | Long m' i' => If m = m'
                                    then (i',v) :: (get_modded_namespace m ns')
                                    else get_modded_namespace m ns'
                   end
   end.
 
-Fixpoint nsLookup {M N V : Type} `{EqDec M eq} `{EqDec N eq} (id : ident M N) (ns : namespace M N V) : option V :=
+Fixpoint nsLookup {M N V : Type} (id : ident M N) (ns : namespace M N V) : option V :=
   match id with
   | Short n => lookup id ns
   | Long m id' => nsLookup id' (get_modded_namespace m ns)
   end.
 
-Fixpoint nsLookupMod {M N V : Type} `{EqDec M} `{EqDec N} (ns : namespace M N V) (xs : list M) : option (namespace M N V) :=
+(* LATER: use LibList.filter *)
+
+Fixpoint nsLookupMod {M N V : Type} (ns : namespace M N V) (xs : list M) : option (namespace M N V) :=
   match xs, ns with
   | _, [] => None
   | [], ns => Some ns
-  | m :: ms, ns => nsLookupMod (filter (fun i => match fst i with
+  | m :: ms, ns => nsLookupMod (filter (fun '(i1,i2) => match i1 with
                                             | Short n    => false
-                                            | Long  m' n => if equiv_dec m m' then true else false
+                                            | Long  m' n => If m = m' then true else false
                                             end)
                                      ns)
                              ms
-  end.
+  end. 
 
-Definition nsEmpty {M N V : Type} := @nil ((ident M N) * V).
+Definition nsEmpty {M N V : Type} := 
+  @nil ((ident M N) * V).
 
 Definition nsAppend {M N V : Type} (ns1 : namespace M N V) (ns2 : namespace M N V) : namespace M N V :=
   ns1 ++ ns2.
 
 Definition nsLift {M N V : Type} (m : M) (ns : namespace M N V) : namespace M N V :=
-  map (fun i => (Long m (fst i), snd i)) ns.
+  map (fun '(i1,i2) => (Long m i1, i2)) ns.
 
 Definition alist_to_ns {M N V : Type} (l : alist N V) : namespace M N V :=
-  map (fun i => (Short (fst i), snd i)) l.
+  map (fun '(i1,i2) => (Short i1, i2)) l.
 
 Definition nsBind {M N V : Type} (n : N) (v : V) (ns : namespace M N V) : namespace M N V :=
   (Short n, v) :: ns.
@@ -125,16 +98,18 @@ Definition nsOptBind {M N V : Type} (n : option N) (v : V) (ns : namespace M N V
   | Some n' => nsBind n' v ns
   end.
 
-Definition nsSing {M N V : Type} (n : N) (v : V) : namespace M N V := [(Short n, v)].
+Definition nsSing {M N V : Type} (n : N) (v : V) : namespace M N V :=
+  [(Short n, v)].
 
+(* LATER: fix this difference *)
 (* First difference here. Using Prop instead of bool. *)
-Definition nsSub {M N V1 V2 : Type} `{EqDec M eq} `{EqDec N eq} `{EqDec V1 eq} `{EqDec V2 eq}
+Definition nsSub {M N V1 V2 : Type}
            (rel : ident M N -> V1 -> V2 -> Prop)
            (ns1 : namespace M N V1)
            (ns2 : namespace M N V2) : Prop :=
-  (forall (id : ident M N) (v1 : V1), nsLookup id ns1 = Some v1 -> exists (v2 : V2), nsLookup id ns2 = Some v2 /\ rel id v1 v2)
-  /\
-  (forall (id : ident M N), nsLookup id ns1 = None -> nsLookup id ns2 = None).
+     (forall (id : ident M N) (v1 : V1), nsLookup id ns1 = Some v1 -> 
+       exists (v2 : V2), nsLookup id ns2 = Some v2 /\ rel id v1 v2)
+  /\ (forall (id : ident M N), nsLookup id ns1 = None -> nsLookup id ns2 = None).
 
 Definition nsAll {M N V : Type} `{EqDec M eq} `{EqDec N eq} `{EqDec V}
            (rel : ident M N -> V -> Prop)
@@ -142,10 +117,11 @@ Definition nsAll {M N V : Type} `{EqDec M eq} `{EqDec N eq} `{EqDec V}
   (forall (id : ident M N) (v : V),
      nsLookup id ns = Some v -> rel id v).
 
-Definition eAll2 {M N V1 V2 : Type} `{EqDec M eq} `{EqDec N eq} `{EqDec V1 eq} `{EqDec V2 eq}
-(rel : ident M N -> V1 -> V2 -> Prop) (ns1 : namespace M N V1) (ns2 : namespace M N V2) : Prop :=
+Definition eAll2 {M N V1 V2 : Type} (rel : ident M N -> V1 -> V2 -> Prop) 
+  (ns1 : namespace M N V1) (ns2 : namespace M N V2) : Prop :=
   nsSub rel ns1 ns2 /\ nsSub (fun x y z => rel x z y) ns2 ns1.
 
+(* LATER: fix this *)
 (* Need to construct a set of id's that are bound. *)
 
 (* Definition nsDom {M N V : Type} `{Eq M} `{Eq N} `{Eq V} SetType V (ns : namespace M N V) : set (Id M N) := *)
@@ -153,7 +129,8 @@ Definition eAll2 {M N V1 V2 : Type} `{EqDec M eq} `{EqDec N eq} `{EqDec V1 eq} `
  (*  nsLookup ns id = Just v *)
  (* { n | forall (v IN universal) (n IN universal) | nsLookup env n = Just v } *)
 
-Definition extractIds {M N V : Type} (ns : namespace M N V) : list (ident M N) := map fst ns.
+Definition extractIds {M N V : Type} (ns : namespace M N V) : list (ident M N) := 
+  map fst ns.
 
 Fixpoint extractMods {M N V : Type} (ns : namespace M N V) : list (list M) :=
   let get_ids := fix gids id :=
@@ -162,7 +139,6 @@ Fixpoint extractMods {M N V : Type} (ns : namespace M N V) : list (list M) :=
                    | Long m id' => m :: gids id'
                    end
   in
-
   match ns with
   | [] => []
   | (id,_)::ns' => get_ids id :: extractMods ns'
@@ -174,4 +150,7 @@ Fixpoint extractMods {M N V : Type} (ns : namespace M N V) : list (list M) :=
 
 Fixpoint nsMap {M N V W : Type} (f : V -> W)
          (ns : namespace M N V) : namespace M N W :=
-  map (fun i => (fst i, f (snd i))) ns.
+  map (fun '(i1,i2) => (i1, f i2)) ns.
+
+(* LATER: use List.map
+   LATER: avoid fst/snd where possible *)
